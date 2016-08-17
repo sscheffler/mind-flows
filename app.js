2c3c7177999ab85d14d0598f2733d5574e5bed21
Stefan Scheffler <stefanscheffler82@gmail.com>
2016-08-17 19:09:27 +0200

    print env

--- app.js
+++ app.js
@@ -11,9 +11,15 @@ var express = require('express');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
+var mongo = process.env.VCAP_SERVICES;
 
 // create a new express server
 var app = express();
+
+
+
+
+
 app.get('/api', function (req, res) {
   res.write('Two APIs are provided: "/api/insertMessage" and "/api/render"' + "\n"
     + 'When "/api/insertMessage" is called, messages will be written to database' + "\n"
@@ -26,6 +32,86 @@ app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+console.log(JSON.stringify(appEnv.getServices()));
+
+
+/*var conn_str = "";
+if (mongo) {
+  var env = JSON.parse(mongo);
+  if (env['mongodb-2.4']) {
+    mongo = env['mongodb-2.4'][0]['credentials'];
+    if (mongo.url) {
+      conn_str = mongo.url;
+    } else {
+      console.log("No mongo found");
+    }  
+  } else {
+    conn_str = 'mongodb://localhost:27017';
+  }
+} else {
+  conn_str = 'mongodb://localhost:27017';
+}
+
+var MongoClient = require('mongodb').MongoClient;
+var db; 
+MongoClient.connect(conn_str, function(err, database) {
+  if(err) throw err;
+  db = database;
+}); 
+
+
+app.get('/api/insertMessage', function (req, res) {
+  var message = { 'message': 'Hello, Bluemix', 'ts': new Date() };
+  if (db && db !== "null" && db !== "undefined") {
+    db.collection('messages').insert(message, {safe:true}, function(err){
+      if (err) { 
+        console.log(err.stack);
+        res.write('mongodb message insert failed');
+        res.end(); 
+      } else {
+        res.write('following messages has been inserted into database' + "\n" 
+        + JSON.stringify(message));
+        res.end();
+      }
+    });    
+  } else {
+    res.write('No mongo found');
+    res.end();
+  } 
+});
+
+app.get('/api/render', function (req, res) {
+  if (db && db !== "null" && db !== "undefined") {
+    // list messages
+    db.collection('messages').find({}, {limit:10, sort:[['_id', 'desc']]}, function(err, cursor) {
+      if (err) {
+        console.log(err.stack); 
+        res.write('mongodb message list failed');
+        res.end();
+      } else {
+        cursor.toArray(function(err, items) {
+          if (err) {
+            console.log(err.stack); 
+            res.write('mongodb cursor to array failed');
+            res.end();
+          } else {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            for (i=0; i < items.length; i++) {
+              res.write(JSON.stringify(items[i]) + "\n");
+            }
+            res.end();
+          }
+        });
+      }
+    });     
+  } else {
+    res.write('No mongo found');
+    res.end();  
+  }
+});*/
+
+
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {

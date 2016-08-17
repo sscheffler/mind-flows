d5f351b8d6aa36e44fd83d35914c69213e72d6e1
Stefan Scheffler <stefanscheffler82@gmail.com>
2016-08-17 18:52:20 +0200

    fixed compile error

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
-pp.get('/api', function (req, res) {
+app.get('/api', function (req, res) {
   res.write('Two APIs are provided: "/api/insertMessage" and "/api/render"' + "\n"
     + 'When "/api/insertMessage" is called, messages will be written to database' + "\n"
     + 'When "/api/render" is called, the inserted message will be shown');

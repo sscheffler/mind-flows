be3697299183a8f6ba9ae237b67469270ae996e6
Stefan Scheffler <stefanscheffler82@gmail.com>
2016-08-20 07:33:45 +0200

    typescript is working

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,30 @@
 'use strict';
 
 var gulp = require('gulp'),
-    gutil = require('gulp-util')
+    sourcemaps = require('gulp-sourcemaps'),
+    babel = require('gulp-babel'),
+    gutil = require('gulp-util'),
+    path = require('path'),
+    ts = require('gulp-typescript'),
+    notify = require('gulp-notify'),
+    runSequence = require('run-sequence')
     ;
+
+var paths = {
+    p: ['server/**/*.js'],
+    server: 'server',
+    client: 'client',
+    dist: 'dist',
+    tsConfig: './tsconfig.json',
+    compassConfig: './config.rb',
+    // Must be absolute or relative to source map
+    sourceRoot: path.join(__dirname, 'p')
+};
 
 
 var tasks = {
     babel: 'babel',
+    build: 'build',
     node: 'node',
     clean: 'clean',
     default: 'default',
@@ -22,9 +40,35 @@ var tasks = {
     browserSync: 'browser-sync'
 };
 
-/**
- * default start task
- */
-gulp.task(tasks.default, function (callback) {
-    gutil.log('default gulp task!');
+var baseTranspileFiles = ['server.ts'];
+gulp.task(tasks.transpile, function () {
+    var tsProject = ts.createProject('./tsconfig.json');
+    return gulp.src(baseTranspileFiles, {base: './'})
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject)) // from typescript to ES6
+        .pipe(babel()) // from ES6 to ES5
+        .pipe(sourcemaps.write())
+        .on('error', handleError)
+        .pipe(gulp.dest(paths.dist));
 });
+
+gulp.task(tasks.build, function () {
+    gutil.log('Building environment');
+    runSequence(
+        tasks.transpile);
+});
+
+
+/**
+ * default task - builds the environment: mainly for calling in bluemix
+ */
+gulp.task(tasks.default, function () {
+    // runSequence();
+});
+
+//---------------------privates----------------------------
+
+function handleError(error) {
+    gutil.log(error.toString());
+    notify(error.toString());
+}
